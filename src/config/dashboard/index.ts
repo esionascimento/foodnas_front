4a84236742c2ebc5ba26d5d6c682911078d139b8
esionascimento <esionascimento@proton.me>
2022-10-29 13:29:51 -0400

    create: config dashboard

---
+++ src/config/dashboard/index.ts
@@ -0,0 +1,6 @@
+import { TypesConfigNavbar } from 'src/@types/dashboard';
+
+export const configNavbarTabs: TypesConfigNavbar[] = [
+  { text: 'Home', href: '#home' },
+  { text: 'Sobre', href: '#about' },
+];
