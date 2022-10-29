33209e83b3a44074e766ef24f4a516d1a1d1175c
esionascimento <esionascimento@proton.me>
2022-10-29 13:30:18 -0400

    refactor: add breakpoint customizado no _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+declare module '@mui/system' {
+  interface BreakpointOverrides {
+    // Your custom breakpoints
+    laptop: true;
+    tablet: true;
+    mobile: true;
+    desktop: true;
+    // Remove default breakpoints
+    xs: false;
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+  }
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider
+        theme={createTheme({
+          breakpoints: {
+            values: {
+              laptop: 1024,
+              tablet: 640,
+              mobile: 0,
+              desktop: 1280,
+            },
+          },
+        })}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
