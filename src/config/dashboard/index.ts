62245a9998a1dd2dc5162c905bff4c965acc3f6b
esionascimento <esionascimento@proton.me>
2022-12-12 07:37:48 -0400

    refactor: configNavbarTabs rotas

--- src/config/dashboard/index.ts
+++ src/config/dashboard/index.ts
@@ -2,7 +2,7 @@ import { PATH_DASHBOARD } from '@Config/paths';
 import { TypesConfigNavbar } from 'src/@types/dashboard';
 
 export const configNavbarTabs: TypesConfigNavbar[] = [
-  { text: 'Home', href: '#home' },
-  { text: 'Sobre', href: '#about' },
+  { text: 'Home', href: `${PATH_DASHBOARD}` },
+  { text: 'Sobre', href: `${PATH_DASHBOARD}/sobre` },
   { text: 'Venda', href: `${PATH_DASHBOARD}/venda` },
 ];
